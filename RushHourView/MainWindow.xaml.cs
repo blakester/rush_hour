9810fad2eee2bc9eae7f05c36deb3ac5ac148714
Blake Burton <blakesterburt@gmail.com>
2019-05-23 13:49:19 -0600

    Working on UI. Basic functionality is there but many changes are likely.

--- RushHourView/MainWindow.xaml.cs
+++ RushHourView/MainWindow.xaml.cs
@@ -25,12 +25,15 @@ namespace RushHour
         private Dictionary<Border, string> vehicleIDs = new Dictionary<Border, string>(32);
         private Dictionary<string, Border> borders = new Dictionary<string, Border>(32);
         private VehicleGrid grid;
+        private int initialConfig = 1;
         
         public MainWindow()
         {
             InitializeComponent();
-            grid = new VehicleGrid("../../../configurations.txt", 1);
+            grid = new VehicleGrid("../../../configurations.txt", initialConfig);
+            configEntryBox.Text = initialConfig.ToString();
             SetGameGrid();
+            //Panel.SetZIndex(solutionMoveButton, -1);
         }
 
         private void SetGameGrid()
@@ -47,12 +50,14 @@ namespace RushHour
             for (int i = 0; i < grid.Columns; i++)
                 gameGrid.ColumnDefinitions.Add(new ColumnDefinition());
 
+            // represent each Vehicle as a Border
             foreach (KeyValuePair<string, Vehicle> kv in grid.vehicles)
             {
                 string vID = kv.Key;
                 Vehicle v = kv.Value;
                 Border vehicleBorder = new Border();
-                vehicleBorder.BorderThickness = new Thickness(10, 10, 10, 10);
+                vehicleBorder.BorderThickness = new Thickness(8, 8, 8, 8);
+                vehicleBorder.CornerRadius = new CornerRadius(15);
 
                 if (vID.Equals("X"))
                     vehicleBorder.Background = Brushes.Red;
@@ -65,102 +70,191 @@ namespace RushHour
                     vehicleBorder.SetValue(Grid.ColumnSpanProperty, v.Length);
 
                 gameGrid.Children.Add(vehicleBorder);
-                vehicleBorder.MouseLeftButtonDown += new MouseButtonEventHandler(Border_MouseLeftButtonDown);
-                //vehicleBorder.AddHandler(Border.MouseLeftButtonDownEvent, new RoutedEventHandler(Border_MouseLeftButtonDown));
+                vehicleBorder.Focusable = true;
 
-                //vehicleBorder.Focusable = true;
-                //vehicleBorder.Focus();
                 //vehicleBorder.MouseLeftButtonDown += new MouseButtonEventHandler(Border_MouseLeftButtonDown);
-                //vehicleBorder.AddHandler(Border.GotKeyboardFocusEvent, new RoutedEventHandler(Border_GotFocus));
+                //vehicleBorder.AddHandler(Border.MouseLeftButtonDownEvent, new RoutedEventHandler(Border_MouseLeftButtonDown));              
+                
+                vehicleBorder.PreviewMouseLeftButtonDown += new MouseButtonEventHandler(Border_PreviewMouseDown);
+                vehicleBorder.AddHandler(Border.GotKeyboardFocusEvent, new RoutedEventHandler(Border_GotFocus));
+
+                vehicleBorder.KeyDown += new KeyEventHandler(border_KeyDown);
 
                 Grid.SetRow(vehicleBorder, v.BackRow);
                 Grid.SetColumn(vehicleBorder, v.BackCol);
-
+                
                 vehicleIDs.Add(vehicleBorder, vID);
                 borders.Add(vID, vehicleBorder);
+                solutionMoveButton.IsEnabled = true;
             }
         }
 
 
-        private void Border_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        //private void Border_MouseLeftButtonDown(object sender, RoutedEventArgs e)
+        //{
+        //    // remove highlighting from selected
+        //    if (selected != null)
+        //        selected.BorderBrush = null;
+        //    selected = (Border)sender;
+        //    selected.BorderBrush = Brushes.Blue;
+        //    selected.Focus();
+        //}
+
+        private void Border_PreviewMouseDown(object sender, MouseButtonEventArgs e)
         {
-            // deselect selected
+            Keyboard.Focus(sender as Border);
+        }
+
+        private void Border_GotFocus(object sender, RoutedEventArgs e)
+        {
+            // remove highlighting from selected
             if (selected != null)
                 selected.BorderBrush = null;
             selected = (Border)sender;
             selected.BorderBrush = Brushes.Blue;
         }
 
-        //private void Border_GotFocus(object sender, RoutedEventArgs e)
-        //{
-        //    // deselect selected
-        //    if (selected != null)
-        //        selected.BorderBrush = null;
-        //    selected = (Border)sender;
-        //    selected.BorderBrush = Brushes.Blue;
-        //}
 
 
-        private void mainWindow_KeyDown(object sender, KeyEventArgs e)
+
+        private void solutionMoveButton_Click(object sender, RoutedEventArgs e)
         {
-            if (selected == null)
-                return;
-            bool vertical = Grid.GetRowSpan(selected) > 1;
+            string movedVehicleID = grid.SolutionNextMove();
+            //if (movedVehicleID == null)
+            //{
+            //    //e.Handled = true;
+            //    if (selected != null)
+            //        selected.Focus();
+            //    return;
+            //}
+
+            Vehicle movedVehicle = grid.vehicles[movedVehicleID];
+            Border movedBorder = borders[movedVehicleID];
+            Grid.SetRow(movedBorder, movedVehicle.BackRow);
+            Grid.SetColumn(movedBorder, movedVehicle.BackCol);            
+            //e.Handled = true;
+            if (selected != null)
+                selected.Focus();
+        }
+
+        private void configButton_Click(object sender, RoutedEventArgs e)
+        {
+            int config = Int32.Parse(configEntryBox.Text); // THIS NEEDS TO BE VALIDATED, OR NON-NUMBERS SHOULD BE PROHIBITIED AT ENTRY
+            grid.SetConfig(config);
+            SetGameGrid();            
+        }
+
+
+        private void border_KeyDown(object sender, KeyEventArgs e)
+        {
+            //if (selected == null)
+            //    return;
+            Border border = (Border)sender;
+            bool vertical = Grid.GetRowSpan(border) > 1;
 
             // get ID of selected Vehicle
-            string vID = vehicleIDs[selected];
+            string vID = vehicleIDs[border];
 
             if (e.Key == Key.Left && !vertical)
             {
                 if (grid.MoveVehicle(vID, -1))
                 {
-                    int destination = Grid.GetColumn(selected) - 1;
-                    Grid.SetColumn(selected, destination);
+                    int destination = Grid.GetColumn(border) - 1;
+                    Grid.SetColumn(border, destination);
+                    solutionMoveButton.IsEnabled = false;
                 }
             }
             else if (e.Key == Key.Right && !vertical)
             {
                 if (grid.MoveVehicle(vID, 1))
                 {
-                    int destination = Grid.GetColumn(selected) + 1;
-                    Grid.SetColumn(selected, destination);
-                }                    
+                    int destination = Grid.GetColumn(border) + 1;
+                    Grid.SetColumn(border, destination);
+                    solutionMoveButton.IsEnabled = false;
+                }
             }
             else if (e.Key == Key.Up && vertical)
             {
                 if (grid.MoveVehicle(vID, -1))
                 {
-                    int destination = Grid.GetRow(selected) - 1;
-                    Grid.SetRow(selected, destination);
-                }                    
+                    int destination = Grid.GetRow(border) - 1;
+                    Grid.SetRow(border, destination);
+                    solutionMoveButton.IsEnabled = false;
+                }
             }
             else if (e.Key == Key.Down && vertical)
             {
                 if (grid.MoveVehicle(vID, 1))
                 {
-                    int destination = Grid.GetRow(selected) + 1;
-                    Grid.SetRow(selected, destination);
+                    int destination = Grid.GetRow(border) + 1;
+                    Grid.SetRow(border, destination);
+                    solutionMoveButton.IsEnabled = false;
                 }
             }
-        }
-
-        private void solutionMoveButton_Click(object sender, RoutedEventArgs e)
-        {
-            string movedVehicleID = grid.SolutionNextMove();
-            if (movedVehicleID == null)
-                return;
-
-            Vehicle movedVehicle = grid.vehicles[movedVehicleID];
-            Border movedBorder = borders[movedVehicleID];
-            Grid.SetRow(movedBorder, movedVehicle.BackRow);
-            Grid.SetColumn(movedBorder, movedVehicle.BackCol);
-        }
-
-        private void configButton_Click(object sender, RoutedEventArgs e)
-        {
-            int config = Int32.Parse(configEntryBox.Text);
-            grid.SetConfig(config);
-            SetGameGrid();            
+            e.Handled = true;
         }
     }
+
+
+
+
+    //private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+    //{
+    //    if (e.Key == Key.Left || e.Key == Key.Right || e.Key == Key.Up || e.Key == Key.Down)
+    //    {
+    //        //configEntryBox.MoveFocus(gameGrid);
+    //        e.Handled = true;
+    //    }
+    //}
+
+
+
+
+
+    //private void mainWindow_KeyDown(object sender, KeyEventArgs e)
+    //{
+    //    if (selected == null)
+    //        return;
+    //    bool vertical = Grid.GetRowSpan(selected) > 1;
+
+    //    // get ID of selected Vehicle
+    //    string vID = vehicleIDs[selected];
+
+    //    if (e.Key == Key.Left && !vertical)
+    //    {
+    //        e.Handled = true;
+    //        if (grid.MoveVehicle(vID, -1))
+    //        {
+    //            int destination = Grid.GetColumn(selected) - 1;
+    //            Grid.SetColumn(selected, destination);
+    //        }
+    //    }
+    //    else if (e.Key == Key.Right && !vertical)
+    //    {
+    //        e.Handled = true;
+    //        if (grid.MoveVehicle(vID, 1))
+    //        {
+    //            int destination = Grid.GetColumn(selected) + 1;
+    //            Grid.SetColumn(selected, destination);
+    //        }                    
+    //    }
+    //    else if (e.Key == Key.Up && vertical)
+    //    {
+    //        e.Handled = true;
+    //        if (grid.MoveVehicle(vID, -1))
+    //        {
+    //            int destination = Grid.GetRow(selected) - 1;
+    //            Grid.SetRow(selected, destination);
+    //        }                    
+    //    }
+    //    else if (e.Key == Key.Down && vertical)
+    //    {
+    //        e.Handled = true;
+    //        if (grid.MoveVehicle(vID, 1))
+    //        {
+    //            int destination = Grid.GetRow(selected) + 1;
+    //            Grid.SetRow(selected, destination);
+    //        }
+    //    }            
+    //}
 }
